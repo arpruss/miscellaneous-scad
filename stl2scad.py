59ee9ec4a9396f718d7688581c77b8a57300a406
arpruss <arpruss@gmail.com>
2016-11-07 23:20:23 -0600

    nicer format

--- stl2scad.py
+++ stl2scad.py
@@ -2,23 +2,31 @@ from stl2svg import loadMesh
 from sys import argv
 
 def toSCAD(polygons):
+
     scad = []
     scad.append('module stlObject1() {polyhedron(')
     scad.append(' points=[')
+    
+    minDimension = min( max(max(v[i] for v in polygon) for polygon in polygons) - 
+        min(min(v[i] for v in polygon) for polygon in polygons) for i in range(3))
+        
+    def fix3(v):
+        return tuple(x if abs(x) > 1e-7 * minDimension else 0. for x in v)
+    
     points = []
     for polygon in polygons:
-        for v in polygon:
-            vv = tuple(v)
+        for i in range(len(polygon)):
+            vv = fix3(v[i])
             if vv not in points:
                 points.append(vv)
-                scad.append('  [%f,%f,%f],' % vv)
+                scad.append('  [%.7g,%.7g,%.7g]' % vv)
     scad.append(' ],')
     scad.append(' faces=[')
     for polygon in polygons:
         p = '  ['
         for v in polygon:
-            vv = tuple(v)
-            p += '%d,' % (points.index(vv),)
+            vv = fix3(v)
+            p += '%d,' % points.index(vv)
         scad.append(p+'],')
     scad.append(' ]);')
     scad.append('}')
