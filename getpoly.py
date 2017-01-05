339de2c89c2621af988b79bc7e164e36a639b231
arpruss <arpruss@gmail.com>
2017-01-05 13:13:48 -0600

    wing

--- getpoly.py
+++ getpoly.py
@@ -3,30 +3,38 @@ import svgpath.parser as parser
 import sys
 import os
 
-def getpoly(filename,precision=0.01):
+def getpoly(filename,precision=0.01,colorExtract=None):
+
+    def colorMatches(c1,c2):
+        if c1 is None or c2 is None:
+            return c1 is c2
+        return max(abs(c1[i]-c2[i]) for i in range(3)) < 0.5/255
+
     with open(filename) as f:
         data = f.read()
         
     svgTree = ET.fromstring(data)
     assert 'svg' in svgTree.tag
+    
+    paths = parser.getPathsFromSVG(svgTree)[0]
 
-    path = parser.getPathsFromSVG(svgTree)[0][0]
+    if colorExtract is None:
+        path = paths[0]
+    else:
+        path = None
+        for p in paths:
+            if colorMatches(p.svgState.stroke, colorExtract):
+                path = p
+
+    assert path != None
+    
     approx = path.linearApproximation(error=precision)
-
-    left = min(min(l.start.real,l.end.real) for l in approx)
-    right = max(max(l.start.real,l.end.real) for l in approx)
-    bottom = min(min(l.start.imag,l.end.imag) for l in approx)
-
-    sx = 1./(right-left)
-
-    def scale(z):
-        return ((z.real-left)*sx,(z.imag-bottom)*sx)
 
     for i in range(len(approx)):
         l = approx[i]
         if i == 0:
-            yield scale(l.start)
-        yield scale(l.end)
+            yield (l.start.real,l.start.imag)
+        yield (l.end.real,l.end.imag)
     
 if __name__=='__main__':
         
--- makewing.py
+++ makewing.py
@@ -1,18 +1,28 @@
 import sys
 from getpoly import getpoly
 
-def makeWing(airfoil, chordScale, thicknessScale, span, slices=100, chordOffset=lambda x:0):
+def getRange(poly):
+    minCoord = tuple(min((p[i] for p in poly)) for i in range(2))
+    maxCoord = tuple(max((p[i] for p in poly)) for i in range(2))
+    return minCoord,maxCoord
+
+def makeWingFromFormulas(airfoil, y, z, span, slices=100):
     points = []
     airfoil = list(airfoil)
+    minCoord,maxCoord = getRange(airfoil)
+    
+    na = len(airfoil)
+    for i in range(len(airfoil)):
+        airfoil[i] = tuple( (airfoil[i][j] - minCoord[j]) / (maxCoord[j] - minCoord[j])  for j in range(2) )
+        
     for slice in range(slices):
         x = span * slice / (slices-1.)
-        ymult = chordScale(x)
-        zmult = thicknessScale(x)
-        yoffset = chordOffset(x)
+        yMinus,yPlus = y(x)
+        zMinus,zPlus = z(x)
         for p in airfoil:
-            points.append( (x,ymult*p[0]+yoffset,zmult*p[1]) )
+            points.append( (x,(yPlus-yMinus)*p[0]+yMinus,(zPlus-zMinus)*p[1]+zMinus) )
+            
     faces = []
-    na = len(airfoil)
     faces.append(range(na))
     for slice in range(slices-1):
         for i in range(na):
@@ -29,15 +39,48 @@ def stringPoly(points,faces):
     out += ']);'
     return out
     
+def toFormula(poly, span):
+    poly = list(poly)
+    minCoord,maxCoord = getRange(poly)
+    
+    scale = float(span)/(maxCoord[0]-minCoord[0])
+
+    def getRangeAtX(x):
+        return min(scale*(p[1]-minCoord[1]) for p in poly if p[0] == x),max(scale*(p[1]-minCoord[1]) for p in poly if p[0] == x)
+    
+    for i in range(len(poly)):
+        poly[i] = tuple( (poly[i][j]-minCoord[j])*scale for j in range(2) )
+        
+    def findAtX(x,span=span,poly=poly):
+        x = min(max(x,span*.0001),span*.9999)
+        
+        minY = float("inf")
+        maxY = float("-inf")
+        n = len(poly)
+        for i,p in enumerate(poly):
+            next = poly[(i+1)%n]
+            if p[0] <= x <= next[0] or next[0] <= x <= p[0]:
+                if p[0] == next[0]:
+                    minY = min(minY, p[1], next[1])
+                    maxY = max(maxY, p[1], next[1])
+                else:
+                    slope = (next[1]-p[1])/(next[0]-p[0])
+                    y = p[1] + slope * (x - p[0])
+                    minY = min(minY, y)
+                    maxY = max(maxY, y)
+        assert minY <= maxY
+        return (minY,maxY)
+
+    return findAtX
+    
+def makeWingFromPolys(airfoil, topView, sideView, span, slices=100):
+    return makeWingFromFormulas(airfoil, toFormula(topView, span), toFormula(sideView, span), span, slices=100)
+    
 if __name__ == '__main__':
     span = 20
-    def chordScale(x):
-        return (x+5) * 4 / (span+5)
-    def thicknessScale(x):
-        if x < 1.05*span * .9:
-            return 1
-        else:
-            return (1.05*span-x) / (1.05*span) / (1-.9)
-    polyhedron = stringPoly(* makeWing(getpoly('airfoil.svg'), chordScale, thicknessScale, span))
+    airfoil = getpoly('airfoil.svg', colorExtract=(0,0,0))
+    topView = getpoly('airfoil.svg', colorExtract=(1,0,0))
+    sideView = getpoly('airfoil.svg', colorExtract=(0,0,1))
+    polyhedron = stringPoly(* makeWingFromPolys(airfoil, topView, sideView, span))
     print('module wing() {\n' + polyhedron + '\n}\n wing();')
     
