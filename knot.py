7fcf00bb1a95e7bd94b3869baf447f5f609d5349
arpruss <arpruss@gmail.com>
2017-01-28 08:59:40 -0600

    allow non-convex sections

--- knot.py
+++ knot.py
@@ -164,6 +164,102 @@ class Matrix(Vector):
         vx = Matrix((0,-v.z,v.y),(v.z,0,-v.x),(-v.y,v.x,0))
         return Matrix.identity(3) + vx + (1./(1+c)) * vx * vx
 
+# triangulation algorithm based on https://www.geometrictools.com/Documentation/TriangulationByEarClipping.pdf
+# minus all the double-linked list stuff that would be great but I am not bothering with it
+
+def cross_z(a,b):
+    # z-component of cross product
+    return a.x * b.y - a.y * b.x
+
+def pointInside(p,a,b,c):
+    c1 = cross_z(p-a, b-a)
+    c2 = cross_z(p-b, c-b)
+    if c1 * c2 < 0:
+        return False
+    c3 = cross_z(p-c, a-c)
+    return c1 * c3 >= 0 and c2 * c3 >= 0
+    
+def triangulate(polygon):
+    # assume input polygon is counterclockwise
+    n = len(polygon)
+
+    if n < 3:
+        raise Exception
+    
+    # two efficient special cases
+    if n == 3:
+        return [(0,1,2)]
+    elif n == 4:
+        return [(0,1,2),(2,3,0)]
+    
+    triangles = []
+    polygon = [Vector(v) for v in polygon]
+    
+    index = list(range(n))
+    
+    def isReflex(i):
+        return cross_z(polygon[i]-polygon[i-1], polygon[(i+1) % n]-polygon[i]) < 0
+        
+    reflex = [isReflex(i) for i in range(n)]
+    
+    def isEar(i):
+        if reflex[i]:
+            return False
+        a,b,c = polygon[i-1],polygon[i],polygon[(i+1) % n]
+        j = i+2
+        while j % n != (i-1) % n:
+            if reflex[j % n] and pointInside(polygon[j % n],a,b,c):
+                return False
+            j += 1
+        return True
+        
+    ear = [isEar(i) for i in range(n)]
+    
+    while n >= 3:
+        i = 0
+        foundEar = False
+        for i in range(n):
+            if ear[i]:
+                triangles.append((index[i-1],index[i],index[(i+1) % n]))
+                # TODO: less deleting!
+                del polygon[i]
+                del reflex[i]
+                del ear[i]
+                del index[i]
+                n -= 1
+                if reflex[i-1]:
+                    reflex[i-1] = isReflex(i-1)
+                if reflex[i % n]:
+                    reflex[i % n] = isReflex(i)
+                if not reflex[i-1]:
+                    ear[i-1] = isEar(i-1)
+                if not reflex[i % n]:
+                    ear[i % n] = isEar(i)
+                foundEar = True
+                break
+            
+        assert foundEar
+        
+    return triangles
+
+def polygonsToSVG(vertices, polys):
+    vertices = tuple(Vector(v) for v in vertices)
+    minX = min(v.x for v in vertices)
+    minY = min(v.y for v in vertices)
+    maxX = max(v.x for v in vertices)
+    maxY = max(v.y for v in vertices)
+
+    svgArray = []
+    svgArray.append('<?xml version="1.0" standalone="no"?>')
+    svgArray.append('<svg width="%fmm" height="%fmm" viewBox="0 0 %f %f" xmlns="http://www.w3.org/2000/svg" version="1.1">'%(maxX-minX,maxY-minY,maxX-minX,maxY-minY))
+    svgArray.append('\n')
+    for p in polys:
+        for i in range(len(p)):
+            svgArray.append('<line x1="%f" y1="%f" x2="%f" y2="%f" stroke="black" stroke-width="0.15px"/>' % 
+                (vertices[p[i-1]].x-minX,vertices[p[i-1]].y-minY,vertices[p[i]].x-minX,vertices[p[i]].y-minY))
+    svgArray.append('</svg>')
+    return '\n'.join(svgArray)
+    
 def toColorSCAD(polys, moduleName="object1"):
     def polyToSCAD(poly):
         pointsDict = {}
@@ -182,7 +278,7 @@ def toColorSCAD(polys, moduleName="object1"):
         out += ");"
         return out
 
-    out = "module() " +moduleName+ " {\n"
+    out = "module " +moduleName+ "() {\n"
     for rgb,monoPolys in polys:
         for poly in monoPolys:
             out += "  color([%.3f,%.3f,%.3f]) " % ( rgb[0]/255., rgb[1]/255., rgb[2]/255. ) 
@@ -258,7 +354,7 @@ class SectionAligner(object):
 
         return out
                 
-def knotMesh(mainPath, section, t1, t2, tstep, upright=Vector(0,0,1), polyhedron=False):
+def knotMesh(mainPath, section, t1, t2, tstep, upright=Vector(0,0,1), solid=False, clockwise=False, scad=False, cacheTriangulation=False, closed=True):
     """
     The upright vector specifies the preferred pointing direction for the y-axis in the input sections.
     The tangent to the mainPain should never be close to parallel to the upright vector. E.g., for a mainly
@@ -266,53 +362,74 @@ def knotMesh(mainPath, section, t1, t2, tstep, upright=Vector(0,0,1), polyhedron
     
     In polyhedron mode, each little piece of the knot is a separate polyhedron, appropriate for dumping into OpenSCAD.
     """
+    
+    if scad:
+        solid = True
+        clockwise = True
+        
+    def orderPolys(polys):
+        if clockwise:
+            return [tuple(reversed(p)) for p in polys]
+        else:
+            return polys
+    
+    cachedTriangulation = None
 
     aligner = SectionAligner(upright=upright)
     
-    def getCrossSection(t):
-        s = section(t)
+    def getCrossSection(s, t):
         f1 = mainPath(t)
         direction = mainPath( (t-t1+tstep/2.)%(t2-t1) + t1 ) - f1
-        return aligner.align(section(t), direction, f1)
+        return aligner.align(s, direction, f1)
         
-    monoMesh = []
+    def getTriangulation(s):
+        if cacheTriangulation:
+            if cachedTriangulation is None:
+                cachedTriangulation = triangulate(s)
+                return cachedTriangulation
+            else:
+                return cachedTriangulation
+        else:
+            return triangulate(s)
+        
+    output = []
     nextCrossSection = None
     t = t1
     while t < t2:
         if nextCrossSection is not None:
-            curCrossSection = nextCrossSection
+            curCrossSection,curTriangulation = nextCrossSection,nextTriangulation
         else:
-            curCrossSection = getCrossSection(t)
-        nextCrossSection = getCrossSection( t+tstep if t+tstep < t2 else t1 )
-        n = min(len(curCrossSection),len(nextCrossSection))
+            s = section(t)
+            curCrossSection,curTriangulation = getCrossSection(s, t), not solid or getTriangulation(s)
+        nextT = t+tstep if t+tstep < t2 else t1
+        s = section(nextT)
+        nextCrossSection,nextTriangulation = getCrossSection( s, nextT ), not solid or getTriangulation(s)
         
-        def triangulateSectionPart(i):
-            return [ ( curCrossSection[i], nextCrossSection[i], nextCrossSection[(i+1)%n] ), 
-                     ( nextCrossSection[(i+1)%n], curCrossSection[(i+1)%n], curCrossSection[i] ) ]
+        n = len(curCrossSection)
+        assert n == len(nextCrossSection)
+        
+        def triangulateTube(i):
+            return orderPolys( [ ( curCrossSection[i], nextCrossSection[i], nextCrossSection[(i+1)%n] ), 
+                     ( nextCrossSection[(i+1)%n], curCrossSection[(i+1)%n], curCrossSection[i] ) ] )
                      
-        def sectionToTriangles(section):
-            n = len(section)
-            center = sum(section) / float(n)
-            out = []
-            for i in range(n):
-                out.append([center,section[i],section[(i+1)%n]])
-            return out
+        def applyTriangulation(points,tr,clockwise=False):
+            if clockwise:
+                return [ tuple(reversed( (points[t[0]],points[t[1]],points[t[2]]))) for t in tr ]
+            else:
+                return [ (points[t[0]],points[t[1]],points[t[2]]) for t in tr ]
 
-        if polyhedron:
-            poly = []
-            poly += sectionToTriangles(list(reversed(curCrossSection[:n])))
-            poly += sectionToTriangles(nextCrossSection[:n])
-            
-            for i in range(n):
-                for tri in triangulateSectionPart(i):
-                    poly.append(list(reversed(tri)))
-            monoMesh.append(poly)
+        if solid:
+            polyhedron = applyTriangulation(curCrossSection, curTriangulation, clockwise=clockwise)
+            polyhedron += applyTriangulation(nextCrossSection, nextTriangulation, clockwise=not clockwise)
+            for i in range(len(curCrossSection)):
+                polyhedron += triangulateTube(i)
+            output.append(polyhedron)
         else:
             for i in range(n):
-                monoMesh += triangulateSectionPart(i)
+                output += triangulateTube(i)
         t += tstep
         
-    return monoMesh
+    return output
     
 if __name__ == '__main__':    
     r = math.sqrt(3)/3.
@@ -321,18 +438,21 @@ if __name__ == '__main__':
     path2 = lambda t: scale*Vector( math.cos(t)+0.5, math.sin(t)-r/2., -math.cos(3*t)/3. )
     path3 = lambda t: scale*Vector( math.cos(t)-0.5, math.sin(t)-r/2, -math.cos(3*t)/3. )
     spin = 1
-    section = lambda t : [cmath.exp(spin*1j*t) * (-.5-.5j),cmath.exp(spin*1j*t) * (-.5+.5j),cmath.exp(spin*1j*t) * (.5+.5j),cmath.exp(spin*1j*t) * (.5-.5j)]
+    
+    #baseSection = Vector( (-.5-.5j),(-.5+.5j),(.5+.5j),(.5-.5j) )
+    baseSection = Vector( cmath.exp(2j*math.pi*k/10) * (1 if k%2 else 0.5) for k in range(10) )
+    section = lambda t : cmath.exp(spin*1j*t) * baseSection
 
     rings = []
-    rings.append( ( (255,0,0), knotMesh(path1, section, 0, 2*math.pi, .05, upright=Vector(0,.1,1), polyhedron=True) ) )
-    rings.append( ( (0,255,0), knotMesh(path2, section, 0, 2*math.pi, .05, upright=Vector(0,.1,1), polyhedron=True) ) )
-    rings.append( ( (0,0,255), knotMesh(path3, section, 0, 2*math.pi, .05, upright=Vector(0,.1,1), polyhedron=True) ) )
+    rings.append( ( (255,0,0), knotMesh(path1, section, 0, 2*math.pi, .1, upright=Vector(0,.1,1), scad=True) ) )
+    rings.append( ( (0,255,0), knotMesh(path2, section, 0, 2*math.pi, .1, upright=Vector(0,.1,1), scad=True) ) )
+    rings.append( ( (0,0,255), knotMesh(path3, section, 0, 2*math.pi, .1, upright=Vector(0,.1,1), scad=True) ) )
 
     saveColorSCAD("rings.scad", rings)
 
     rings = []
-    rings.append( ( (255,0,0), knotMesh(path1, section, 0, 2*math.pi, .05, upright=Vector(0,.1,1), polyhedron=False) ) )
-    rings.append( ( (0,255,0), knotMesh(path2, section, 0, 2*math.pi, .05, upright=Vector(0,.1,1), polyhedron=False) ) )
-    rings.append( ( (0,0,255), knotMesh(path3, section, 0, 2*math.pi, .05, upright=Vector(0,.1,1), polyhedron=False) ) )
+    rings.append( ( (255,0,0), knotMesh(path1, section, 0, 2*math.pi, .1, upright=Vector(0,.1,1), scad=False) ) )
+    rings.append( ( (0,255,0), knotMesh(path2, section, 0, 2*math.pi, .1, upright=Vector(0,.1,1), scad=False) ) )
+    rings.append( ( (0,0,255), knotMesh(path3, section, 0, 2*math.pi, .1, upright=Vector(0,.1,1), scad=False) ) )
 
     saveColorSTL("rings.stl", rings)
