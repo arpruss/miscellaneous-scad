45c989a74948cbe82c7c9e0612f53df98535d989
arpruss <arpruss@gmail.com>
2017-04-01 12:48:35 -0500

    foo

---
+++ resolve-include.py
@@ -0,0 +1,37 @@
+import sys
+import re
+
+output = []
+params = []
+skip = []
+current = output
+
+stack = []
+
+def process(name):
+    global current
+    with open(name) as f:
+        for line in f:
+            m = re.match(r'(include|use)\s+<([^>]+)>', line)
+            if m:
+                current.append("//"+line.strip())
+                process(m.group(2))
+            else:
+                lineStripped = line.strip()
+                if lineStripped == "//<params>":
+                    stack.append(current)
+                    current = params
+                elif lineStripped == "//</params>":
+                    current = stack.pop()
+                elif lineStripped == "//<skip>":
+                    stack.append(current)
+                    current = skip
+                elif lineStripped == "//</skip>":
+                    current = stack.pop()
+                else:
+                    current.append(line.rstrip())
+                
+process(sys.argv[1])
+if params:
+    params.append("\nmodule end_of_parameters_dummy() {}\n")
+print "\n".join(params+output)
