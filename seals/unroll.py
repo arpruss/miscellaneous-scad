f97b4aa8ef75c4aeeed1cd2472322c93ede4fb4c
arpruss <arpruss@gmail.com>
2018-07-28 09:54:58 -0500

    unroll

--- seals/unroll.py
+++ seals/unroll.py
@@ -1,6 +1,7 @@
 from sys import *
 from math import *
 from random import *
+import subprocess
 
 thickness = 10
 maxR = 0
@@ -9,10 +10,12 @@ sumR = 0
 count = 0
 points = []
 
+out = argv[1] + "-unroll" if not argv[1].lower().endswith(".txt") else argv[1][:-4]+"-unroll"
+
 with open(argv[1]) as f:
     for line in f:
         try:
-            v = tuple(float(x) for x in line.split(",")[:3])
+            v = tuple(float(x) for x in line.split(";")[:3])
             if len(v) < 3:
                 raise Error()
             points.append(v)
@@ -33,17 +36,20 @@ maxZ = max(v[2] for v in points)
 stderr.write("Min Z = %.2f, max Z = %.2f\n" % (minZ,maxZ))
 stderr.write("Average R = %.2f, min R = %.2f, max R = %.2f\n" % (avgR,minR,maxR))
 
-for v in points:
-    try:
-        angle = atan2(v[1],v[0])
-    except:
-        continue
-    r = sqrt(v[0]*v[0]+v[1]*v[1])
-    x,y,z=angle*avgR,v[2],thickness+maxR-r
-    if x < 0:
-        x += 2*pi*avgR
-    x1 = x + 2*pi*avgR
-    print("%.5f,%.5f,%.5f" % (x,y,z))
-    print("%.5f,%.5f,%.5f" % (x1,y,z))
+with open(out+".txt", "w") as f: 
+    for v in points:
+        try:
+            angle = atan2(v[1],v[0])
+        except:
+            continue
+        r = sqrt(v[0]*v[0]+v[1]*v[1])
+        x,y,z=angle*avgR,v[2],thickness+maxR-r
+        if x < 0:
+            x += 2*pi*avgR
+        x1 = x + 2*pi*avgR
+        f.write("%.5f;%.5f;%.5f\n" % (x,y,z))
+        f.write("%.5f;%.5f;%.5f\n" % (x1,y,z))
 
-stderr.write("0<=x && x<=%.5f && %.5f<=y && y<=%.5f" % (4*pi*avgR,minZ,maxZ))
+stderr.write("0<=x && x<=%.5f && %.5f<=y && y<=%.5f\n" % (4*pi*avgR,minZ,maxZ))
+subprocess.call(["meshlabserver", "-i", out+".txt", "-s", "process_unroll.mlx", "-o", out+".stl"])
+subprocess.call(["pypy", "../scripts/slabify.py", out+".stl", "10", "0", str(minZ), str(4*pi*avgR), str(maxZ)])
