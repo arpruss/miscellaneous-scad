96bb866767652d350c18295a6aff9be2d3771b32
arpruss <arpruss@gmail.com>
2018-07-28 08:59:05 -0500

    relief

---
+++ scripts/relief.py
@@ -0,0 +1,32 @@
+#
+# magnify relief to an engraved cylinder
+#
+# python relief.py filename.stl factor
+#
+
+from loadmesh import loadMesh
+from sys import argv, stderr
+from exportmesh import *
+from math import *
+
+mesh = loadMesh(argv[1], reverseTriangles=False)
+relief = float(argv[2])
+rlf = "-relief%g.stl" % relief
+out = argv[1] + rlf if not argv[1].lower().endswith(".stl") else argv[1][:-4] + rlf
+
+def radius(v):
+    return sqrt(v[0]*v[0]+v[1]*v[1])
+    
+print(len(mesh))
+maxR = max(radius(v) for tri in mesh for v in tri)
+stderr.write("maxR = %.2f\n" % maxR)
+
+mesh2 = []
+
+for tri in mesh:
+    def fixVertex(v):
+        a = (radius(v)/maxR)**(relief-1)
+        return (v[0]*a,v[1]*a,v[2])
+    mesh2.append(tuple( fixVertex(v) for v in tri ))
+
+saveSTL(out, mesh2)                       
