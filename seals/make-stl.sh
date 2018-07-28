e9a6d1b94d46c401345843c763c91d38a46d1401
arpruss <arpruss@gmail.com>
2018-07-28 09:01:21 -0500

    gen

--- seals/make-stl.sh
+++ seals/make-stl.sh
@@ -1,4 +1,6 @@
 for x in VA*.txt ; do
     echo $x
-    meshlabserver -i $x -o ${x%.*}.stl -s process_seal.mlx
+#    meshlabserver -i $x -o ${x%.*}.stl -s process_seal.mlx
+    pypy ../scripts/relief ${x%.*}.stl 1.5
+    pypy ../scripts/relief ${x%.*}.stl 2
 done
