dab52d7f18195fe5289ca753c6f5c032db46a0d4
arpruss <arpruss@gmail.com>
2018-07-28 12:05:08 -0500

    mogrify

--- seals/make-stl.sh
+++ seals/make-stl.sh
@@ -1,9 +1,11 @@
-for x in VA*3D_[0-9][0-9][0-9][0-9].txt ; do
+for x in VA[0-9][0-9][0-9][0-9][0-9].txt ; do
     echo $x
-#    meshlabserver -i $x -o ${x%.*}.stl -s process_seal.mlx
-#    pypy ../scripts/relief.py ${x%.*}.stl 1.5
-#    pypy ../scripts/relief.py ${x%.*}.stl 2
-#    pypy unroll.py $x
+    meshlabserver -i $x -o ${x%.*}.stl -s process_seal.mlx
+    pypy ../scripts/relief.py ${x%.*}.stl 1.5
+    pypy ../scripts/relief.py ${x%.*}.stl 2
+    pypy unroll.py $x
     echo 'color("gray") import("'${x%.*}-unroll-slab.stl'");' > ${x%.*}-unroll.scad
-    openscad --camera 50,0,300,50,0,0 --autocenter --imgsize=1024,500 -o ${x%.*}.png ${x%.*}-unroll.scad 
+    echo 'translate([0,-30,0]) text("'${x%.*}$'",size=8);' >> ${x%.*}-unroll.scad
+    openscad --camera 80,0,200,80,0,0 --autocenter --imgsize=1600,600 -o ${x%.*}.png ${x%.*}-unroll.scad 
+    mogrify -trim ${x%.*}.png
 done
